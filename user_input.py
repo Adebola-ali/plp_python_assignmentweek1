b98dcec5223ff4c61c5d29ac04dc63761c003812
Oguntade Adebola A <adebolabiz@gmail.com>
2024-10-31 16:10:54 +0100

    initial commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Request user information using input() function
+
+name = input("What's your fullname? ")
+age = input("How old are you? ")
+location = input("where is your location? ")
+print(f"Hello, {name}! You are {age} years old, you live in {location} Nigeria")
